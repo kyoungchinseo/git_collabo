e48cb3119f2bcdaceeb74e99537f1c35fb592956
kyoungchin <kcseo.acm@gmail.com>
2016-09-11 02:32:32 +0900

    add cool thing

--- main.c
+++ main.c
@@ -6,5 +6,15 @@ int main(int argc, char[] argv)
 {
 	printf("hello world\n");
 	int a= 0;
-	printtf("%d\n",a);
+	printf("%d\n",a);
+
+	int x = 10;
+	int y = 0;
+
+	while(x > 3) {
+		y = y + x;
+		x = x - 1;
+		printf("x: %d, y: %d\n",x,y);
+	}
+
 }
